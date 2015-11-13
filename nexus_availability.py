02e527b4f204d1e8b3c60f82ed5f4ecee689cbec
Daniel “dictvm” Heitmann <dictvm666@gmail.com>
2015-11-13 23:53:01 +0100

    cleaned up the vars, notifications are only sent once

--- nexus_availability.py
+++ nexus_availability.py
@@ -3,6 +3,7 @@
 
 import requests
 import os
+import time
 
 from sys import exit
 from lxml import html
@@ -10,17 +11,24 @@ from pushbullet import Pushbullet
 
 api_key = os.getenv('PB_API_KEY', 0)
 pb = Pushbullet(api_key)
-phone = os.getenv('NEXUS_VARIANT', 0)
+nexus_device = os.getenv('NEXUS_VARIANT', 0)
 store_url = "https://store.google.com/product/nexus_"
-store = requests.get(store_url+phone)
-tree = html.fromstring(store.text)
+store_content = requests.get(store_url+nexus_device)
+store_tree = html.fromstring(store_content.text)
 notification_title = "Google Store Checker"
-notification_body = "Order your Nexus "+phone.upper()+" now!"
-availability = tree.xpath('//p[@class="out-of-stock-text"]/text()')
+notification_body = "Order your Nexus "+nexus_device.upper()+" now!"
+notification_triggered = 0
+availability = store_tree.xpath('//p[@class="out-of-stock-text"]/text()')
 
-if len(availability) > 0 and "Bald" in availability[0]:
-    print("Google Store Checker: Not available yet.")
-elif len(availability) > 0 and "soon" in availability[0]:
-    print("Google Store Checker: Not available yet.")
-else:
-    push = pb.push_note(notification_title, notification_body)
+while notification_triggered == 0:
+    if len(availability) > 0 and "Bald" in availability[0]:
+        print("Google Store Checker: Not available yet.")
+        time.sleep(120)
+    elif len(availability) > 0 and "soon" in availability[0]:
+        print("Google Store Checker: Not available yet.")
+        time.sleep(120)
+    else:
+        # make sure to only notify once
+        print(notification_body)
+        push = pb.push_note(notification_title, notification_body)
+        notification_triggered = 1
