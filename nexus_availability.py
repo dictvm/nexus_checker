82b497cd8f2650670be09ab7aff1d7a4f1963c99
Daniel “dictvm” Heitmann <dictvm666@gmail.com>
2015-11-14 02:02:11 +0100

    moved store check into while loop

--- nexus_availability.py
+++ nexus_availability.py
@@ -9,18 +9,20 @@ from sys import exit
 from lxml import html
 from pushbullet import Pushbullet
 
+notification_triggered = 0
 api_key = os.getenv('PB_API_KEY', 0)
 pb = Pushbullet(api_key)
 nexus_device = os.getenv('NEXUS_VARIANT', 0)
 store_url = "https://store.google.com/product/nexus_"
-store_content = requests.get(store_url+nexus_device)
-store_tree = html.fromstring(store_content.text)
-notification_title = "Google Store Checker"
-notification_body = "Order your Nexus "+nexus_device.upper()+" now!"
-notification_triggered = 0
-availability = store_tree.xpath('//p[@class="out-of-stock-text"]/text()')
 
 while notification_triggered == 0:
+    # refresh of store content needs to happen in the loop
+    store_content = requests.get(store_url+nexus_device)
+    store_tree = html.fromstring(store_content.text)
+    notification_title = "Google Store Checker"
+    notification_body = "Order your Nexus "+nexus_device.upper()+" now!"
+    availability = store_tree.xpath('//p[@class="out-of-stock-text"]/text()')
+
     if len(availability) > 0 and "Bald" in availability[0]:
         print("Google Store Checker: Not available yet.")
         time.sleep(120)
