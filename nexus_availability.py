a0b20009aeb8c00a2d99a4696031d41e92a68940
Daniel “dictvm” Heitmann <dictvm666@gmail.com>
2015-11-19 23:42:50 +0100

    code trimming, optimized loop, use native store availability check

--- nexus_availability.py
+++ nexus_availability.py
@@ -9,28 +9,26 @@ from sys import exit
 from lxml import html
 from pushbullet import Pushbullet
 
-#notification_triggered = 0
 api_key = os.getenv('PB_API_KEY', 0)
 pb = Pushbullet(api_key)
-nexus_device = os.getenv('NEXUS_VARIANT', 0)
+nexus_device = os.getenv('NEXUS_DEVICE', 0)
+nexus_variant = os.getenv('NEXUS_VARIANT', 0)
 store_url = "https://store.google.com/product/nexus_"
 
-while True:
-    # refresh of store content needs to happen in the loop
-    store_content = requests.get(store_url+nexus_device)
-    store_tree = html.fromstring(store_content.text)
-    notification_title = "Google Store Checker"
-    notification_body = "Order your Nexus "+nexus_device.upper()+" now!"
-    availability = store_tree.xpath('//p[@class="out-of-stock-text"]/text()')
+def main():
 
-    if len(availability) > 0 and "Bald" in availability[0]:
+    while True:
+        store_content = requests.get(store_url+nexus_device)
+        store_tree = html.fromstring(store_content.text)
+        notification_title = "Google Store Checker"
+        notification_body = "Order your Nexus {} now!".format(nexus_device.upper())
+        for e in store_tree.xpath('//*[@data-variation-name="{}"]'.format(nexus_variant)):
+            print(e.getparent().attrib["data-available"])
+            if e.getparent().attrib["data-available"] == "true":
+                print(notification_body)
+                push = pb.push_note(notification_title,  notification_body)
+                return
         print("Google Store Checker: Not available yet.")
         time.sleep(120)
-    elif len(availability) > 0 and "soon" in availability[0]:
-        print("Google Store Checker: Not available yet.")
-        time.sleep(120)
-    else:
-        # make sure to only notify once
-        print(notification_body)
-        push = pb.push_note(notification_title, notification_body)
-        break
+
+main()
