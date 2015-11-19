f3f0be4dad36658448943111c758f4ff84520be8
Daniel “dictvm” Heitmann <dictvm666@gmail.com>
2015-11-19 22:30:21 +0100

    improved while loop

--- nexus_availability.py
+++ nexus_availability.py
@@ -9,13 +9,13 @@ from sys import exit
 from lxml import html
 from pushbullet import Pushbullet
 
-notification_triggered = 0
+#notification_triggered = 0
 api_key = os.getenv('PB_API_KEY', 0)
 pb = Pushbullet(api_key)
 nexus_device = os.getenv('NEXUS_VARIANT', 0)
 store_url = "https://store.google.com/product/nexus_"
 
-while notification_triggered == 0:
+while True:
     # refresh of store content needs to happen in the loop
     store_content = requests.get(store_url+nexus_device)
     store_tree = html.fromstring(store_content.text)
@@ -33,4 +33,4 @@ while notification_triggered == 0:
         # make sure to only notify once
         print(notification_body)
         push = pb.push_note(notification_title, notification_body)
-        notification_triggered = 1
+        break
